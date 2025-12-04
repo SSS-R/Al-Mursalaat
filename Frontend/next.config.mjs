782f7f42db528b0c6a9b989aad8f898900457365
Sultan Rafi <sultan.rafi091@gmail.com>
2025-12-05 00:21:36 +0600

    feat: Add API rewrites for proxying requests to backend server

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path',
+        destination: 'http://192.168.0.231:8000/api/:path', // Proxy to your backend server
+      },
+    ];
+  },
 };
 
 export default nextConfig;
