fe860542d45086d8dc939a21a209fe48038b6b28
Sultan Rafi <sultan.rafi091@gmail.com>
2026-01-28 01:57:32 +0600

    typo fix

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -9,26 +9,27 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://127.0.0.1:8000/api/:path*',
-        },
-        {
-          source: '/docs',
-          destination: 'http://127.0.0.1:8000/docs',
-        },
-        {
-          source: '/openapi.json',
-          destination: 'http://127.0.0.1:8000/openapi.json',
-        },
-        {
-          source: '/submit-application',
-          destination: 'http://127.0.0.1:8000/submit-application/',
-        },
-      ]
-    },
-  };
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
+      },
+      {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8000/docs',
+      },
+      {
+        source: '/openapi.json',
+        destination: 'http://127.0.0.1:8000/openapi.json',
+      },
+      {
+        source: '/submit-application',
+        destination: 'http://127.0.0.1:8000/submit-application/',
+      },
+    ]
+  },
+};
 
-  export default nextConfig;
+export default nextConfig;
