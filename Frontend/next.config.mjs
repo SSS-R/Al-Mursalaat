9e4988588d48b9f154e5c83327169a9bbc54de44
Sultan Rafi <sultan.rafi091@gmail.com>
2026-01-28 23:17:28 +0600

    routing fix

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -10,6 +10,22 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8000/docs',
+      },
+      {
+        source: '/openapi.json',
+        destination: 'http://127.0.0.1:8000/openapi.json',
+      },
+      {
+        source: '/submit-application/',
+        destination: 'http://127.0.0.1:8000/submit-application/',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
