03da56526888b9c2655dbb2af1afea9ba4c96061
Sultan Rafi <sultan.rafi091@gmail.com>
2026-01-28 22:56:57 +0600

    feat: Add Next.js configuration to ignore ESLint/TypeScript build errors and unoptimize images.

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -10,26 +10,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
-      },
-      {
-        source: '/docs',
-        destination: 'http://127.0.0.1:8000/docs',
-      },
-      {
-        source: '/openapi.json',
-        destination: 'http://127.0.0.1:8000/openapi.json',
-      },
-      {
-        source: '/submit-application',
-        destination: 'http://127.0.0.1:8000/submit-application/',
-      },
-    ]
-  },
 };
 
 export default nextConfig;
